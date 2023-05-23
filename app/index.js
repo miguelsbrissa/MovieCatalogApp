43f359fdb002be75fa9deed94ca011b52b6da80a
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-05-23 18:39:58 -0300

    feat: :sparkles: Add components and style to the home screen

--- app/index.js
+++ app/index.js
@@ -1,12 +1,37 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import styles  from '../styles/style'
+import { View, Text, SafeAreaView, ScrollView, TextInput } from 'react-native'
+import { Stack, useRouter } from 'expo-router'
+import styles from '../styles/style'
+import { COLORS, SIZES } from '../constants'
+import { Feather } from '@expo/vector-icons'
+import { Welcome, SearchBarHeader } from '../components'
 
 const Home = () => {
+  const { homeHeaderWrapper } = styles
   return (
-    <View>
-      <Text>Luke Skywalker!</Text>
-    </View>
+    <SafeAreaView>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.primary
+          },
+          headerShadowVisible: false,
+          headerTitle: '',
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            color: COLORS.white
+          },
+          statusBarTranslucent: true,
+          headerLeft: () => (
+            <Feather name={'menu'} size={SIZES.xxLarge} color={COLORS.white} />
+          )
+        }}
+      />
+      <View style={homeHeaderWrapper}>
+        <Welcome name={'Luke Skywalker'} />
+        <SearchBarHeader />
+      </View>
+    </SafeAreaView>
   )
 }
 
--- styles/style.js
+++ styles/style.js
@@ -2,6 +2,11 @@ import { StyleSheet } from 'react-native'
 
 import { COLORS, FONT, SIZES } from '../constants'
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    homeHeaderWrapper:{
+        backgroundColor: COLORS.primary,
+        borderRadius: SIZES.large
+    }
+})
 
 export default styles
