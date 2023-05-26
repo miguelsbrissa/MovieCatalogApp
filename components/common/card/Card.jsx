c26cc1da06509948810c1634d28198535443173e
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-05-26 08:08:02 -0300

    feat: :sparkles: Make card dynamic

--- components/common/card/Card.jsx
+++ components/common/card/Card.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { Text, TouchableOpacity, Image } from 'react-native'
 import styles from './cardStyle'
+import { checkImageURL } from '../../../utils/index'
 
 const Card = ({ item }) => {
   const { container, movieImg, movieTitle } = styles
+
   return (
     <TouchableOpacity style={container}>
-      <Text style={movieTitle} numberOfLines={1}>Spider-Man: Into the Spider-Verse</Text>
-      <Image source={{ uri: item }} resizeMode={'cover'} style={movieImg} />
+      <Text style={movieTitle} numberOfLines={1}>
+        {item.titleText.text}
+      </Text>
+      <Image
+        source={{
+          uri: checkImageURL(item.primaryImage.url)
+            ? item.primaryImage.url
+            : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg'
+        }}
+        resizeMode={'cover'}
+        style={movieImg}
+      />
     </TouchableOpacity>
   )
 }
