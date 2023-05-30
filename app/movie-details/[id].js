4d19b73b212f97d32dac1cddfd5ad8767696a0c4
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-05-30 15:02:00 -0300

    feat: :sparkles: Adds components, style and dynamic content

--- app/movie-details/[id].js
+++ app/movie-details/[id].js
@@ -1,11 +1,64 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, ActivityIndicator } from 'react-native'
+import { useSearchParams, Stack } from 'expo-router'
+import { Feather } from '@expo/vector-icons'
+import { COLORS, SIZES } from '../../constants'
+import useFetch from '../../hooks/useFetch'
+import { HeaderMovie } from '../../components'
 
 const MovieDetails = () => {
+  const params = useSearchParams()
+  const {
+    data: dataMovieDetail,
+    isLoading: isLoadingMovieDetail,
+    //refetch: refetchMovieDetail,
+    error: errorMovieDetail
+  } = useFetch(`titles/${params.id}`, {
+    info: 'base_info'
+  })
+
   return (
-    <View>
-      <Text>MovieDetails</Text>
-    </View>
+    <SafeAreaView>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.primary
+          },
+          headerShadowVisible: false,
+          headerTitle: '',
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            color: COLORS.white
+          },
+          statusBarTranslucent: true,
+          headerLeft: () => (
+            <Feather
+              name={'chevron-left'}
+              size={SIZES.xxLarge}
+              color={COLORS.white}
+            />
+          ),
+          headerRight: () => (
+            <Feather
+              name={'share-2'}
+              size={SIZES.xxLarge}
+              color={COLORS.white}
+            />
+          )
+        }}
+      />
+      <View>
+        {isLoadingMovieDetail ? (
+          <ActivityIndicator size={'large'} color={COLORS.primary} />
+        ) : errorMovieDetail ? (
+          <Text>Something went wrong!</Text>
+        ) : dataMovieDetail.results === undefined ? (
+          <ActivityIndicator size={'large'} color={COLORS.primary} />
+        ) : (
+          <HeaderMovie data={dataMovieDetail.results} />
+        )}
+      </View>
+    </SafeAreaView>
   )
 }
 
