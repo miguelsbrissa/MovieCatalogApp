5f26c442f382d5e00d4f95c9abab3a9e8ad76e68
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-06-02 10:54:18 -0300

    feat: :sparkles: Adds util to convert seconds to hour

--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,18 @@
 export const checkImageURL = (image) => {
-    if (image === null) return false
-    else {
-      const pattern = new RegExp(
-        '^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$',
-        'i'
-      )
-      return pattern.test(image.url)
-    }
+  if (image === null) return false
+  else {
+    const pattern = new RegExp(
+      '^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$',
+      'i'
+    )
+    return pattern.test(image.url)
   }
-  
+}
+
+export const convertSecondsToHours = (seconds) => {
+  let minutes = seconds / 60
+  let hours = Math.floor(minutes / 60)
+  minutes = minutes - (hours * 60)
+
+  return `${hours}h${minutes}min`
+}
