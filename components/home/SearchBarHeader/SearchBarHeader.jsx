9e46292faf917cd984911cc459e141caf885517c
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-06-05 14:25:57 -0300

    feat: :sparkles: Adds functionality to search input in home screen

--- components/home/SearchBarHeader/SearchBarHeader.jsx
+++ components/home/SearchBarHeader/SearchBarHeader.jsx
@@ -1,17 +1,28 @@
 import { View, TextInput } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './searchBarHeaderStyles'
 import { Feather } from '@expo/vector-icons'
 import { COLORS, SIZES } from '../../../constants'
+import { useRouter } from 'expo-router'
 
 const SearchBarHeader = () => {
-  const { container, textInput, iconWrapper, center} = styles
+  const { container, textInput, iconWrapper, center } = styles
+  const [searchValue, setSearchValue] = useState('')
+  const router = useRouter()
+  const handleEnd = () => {
+    if (searchValue) router.push(`search/${searchValue}`)
+  }
+
   return (
     <View style={[container, center]}>
       <View style={[iconWrapper('left'), center]}>
         <Feather name={'search'} size={SIZES.xxLarge} color={COLORS.white} />
       </View>
-      <TextInput style={textInput} />
+      <TextInput
+        style={textInput}
+        onEndEditing={handleEnd}
+        onChangeText={(text) => setSearchValue(text)}
+      />
       <View style={[iconWrapper('right'), center]}>
         <Feather name={'mic'} size={SIZES.xxLarge} color={COLORS.white} />
       </View>
