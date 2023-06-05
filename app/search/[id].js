d777da2a7ac8b47207604738cc6b887d0ed35cdd
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-06-05 15:03:02 -0300

    feat: :sparkles: Creates Search screen

---
+++ app/search/[id].js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Stack, useRouter, useSearchParams } from 'expo-router'
+import { View, Text, SafeAreaView, ActivityIndicator } from 'react-native'
+import { COLORS, SIZES } from '../../constants'
+import { ButtonIcon, SearchList } from '../../components'
+import useFetch from '../../hooks/useFetch'
+
+const Search = () => {
+  const params = useSearchParams()
+  const router = useRouter()
+  const { data, error, isLoading } = useFetch(
+    `titles/search/title/${params.id}`,
+    {
+      exact: 'false',
+      titleType: 'movie',
+      list: 'most_pop_movies'
+    }
+  )
+  return (
+    <SafeAreaView>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.primary
+          },
+          headerShadowVisible: false,
+          headerTitle: `Search for: ${params.id}`,
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            color: COLORS.white
+          },
+          statusBarTranslucent: true,
+          headerLeft: () => (
+            <ButtonIcon
+              iconName={'chevron-left'}
+              iconColor={COLORS.white}
+              iconSize={SIZES.xxLarge}
+              onPress={() => router.back()}
+            />
+          )
+        }}
+      />
+      <View style={{ marginTop: 20 }}>
+        {isLoading ? (
+          <ActivityIndicator size={'large'} color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong!</Text>
+        ) : data.results === undefined ? (
+          <ActivityIndicator size={'large'} color={COLORS.primary} />
+        ) : (
+          <SearchList data={data.results} />
+        )}
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default Search
